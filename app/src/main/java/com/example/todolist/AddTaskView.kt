3fb830db6ab0f189ad19887a7f09005a4fb1aceb
DanielPinoPena <danielpinop2000@gmail.com>
2025-05-20 15:05:42 -0400

    I fixed the design a little bit

--- app/src/main/java/com/example/todolist/AddTaskView.kt
+++ app/src/main/java/com/example/todolist/AddTaskView.kt
@@ -5,7 +5,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
@@ -16,6 +19,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.todolist.data.Task
 import java.util.Date
@@ -128,7 +132,7 @@ fun AddTaskView(
                     }
                 }
             ) {
-                Text("Save")
+                Icon(Icons.Default.Check, contentDescription = null, tint = Color.White)
             }
         }
     }
--- app/src/main/java/com/example/todolist/HomeView.kt
+++ app/src/main/java/com/example/todolist/HomeView.kt
@@ -1,7 +1,9 @@
 package com.example.todolist
 
+import android.graphics.Paint
 import android.widget.Toast
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,6 +11,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
@@ -32,6 +36,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -74,7 +79,7 @@ fun HomeView(navController: NavHostController, viewModel: TaskViewModel) {
             .padding(it)) {
             items(taskList.value, key = {task-> task.id}) {
                 task ->
-                TaskItem(task, viewModel) {
+                TaskItem(task, viewModel, 1) {
                     id = task.id
                     taskBeingEdited = true
                     showBottomSheet = true
@@ -108,57 +113,62 @@ fun HomeView(navController: NavHostController, viewModel: TaskViewModel) {
 }
 
 @Composable
-fun TaskItem(task: Task, viewModel: TaskViewModel, onClick: () -> Unit) {
+fun TaskItem(task: Task, viewModel: TaskViewModel, mode: Int, onClick: () -> Unit) {
     var isChecked by remember { mutableStateOf(false) }
 
-    Card(modifier = Modifier
-        .fillMaxWidth()
-        .padding(top = 8.dp, start = 8.dp, end = 8.dp)
-        .clickable {
-            onClick()
-        },
-        backgroundColor = Color.White
+    val elevationValue = if (mode == 1) 8.dp else 0.dp
 
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 8.dp, start = 8.dp, end = 8.dp)
+            .clickable { onClick() },
+        backgroundColor = colorResource(id = R.color.light_gray),
+        elevation = elevationValue
     ) {
-        Column(
-            modifier = Modifier.padding(16.dp)
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            verticalAlignment = Alignment.Top
         ) {
-            // Checkbox in the top-left corner
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.Top
-            ) {
-                Checkbox(
-                    checked = false,
-                    onCheckedChange = { checked ->
-                        isChecked = checked
-                        if (checked) {
-                            viewModel.deleteTask(task)
-                        }
-                    },
-                    enabled = true
+            Spacer(modifier = Modifier.width(6.dp))
+
+            Checkbox(
+                checked = isChecked,
+                onCheckedChange = { checked ->
+                    isChecked = checked
+                    if (checked) {
+                        viewModel.deleteTask(task)
+                    }
+                },
+                modifier = Modifier
+                    .size(24.dp)
+                    .padding(end = 8.dp)
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = task.title,
+                    style = MaterialTheme.typography.h6
                 )
+                Text(
+                    text = task.description,
+                    style = MaterialTheme.typography.body2
+                )
+
+                if (mode == 2) {
+                    Divider(
+                        modifier = Modifier.padding(top = 8.dp),
+                        color = MaterialTheme.colors.onSurface.copy(alpha = 0.12f),
+                        thickness = 1.dp
+                    )
+                }
             }
-
-            // Task title and description
-            Text(
-                text = task.title,
-                style = MaterialTheme.typography.h6
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = task.description,
-                style = MaterialTheme.typography.body2
-            )
-
-            // Separator Line
-            Divider(
-                modifier = Modifier.padding(top = 8.dp),
-                color = MaterialTheme.colors.onSurface.copy(alpha = 0.12f),
-                thickness = 1.dp
-            )
         }
-
-
     }
 }
+
